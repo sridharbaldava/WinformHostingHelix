05afa4047b1ef46a12a8de73243013482858c9ef
Sridhar Baldava <SBaldava@walterpmoore.com>
2017-11-22 11:43:27 -0600

    initial commit

--- WinformHostingHelix/Form1.Designer.cs
+++ WinformHostingHelix/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent( )
         {
-            this.components = new System.ComponentModel.Container( );
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(238, 222);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WinformHostingHelix/Form1.cs
+++ WinformHostingHelix/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HelixToolkit.Wpf.SharpDX;
+using SharpDX;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 
 namespace WinformHostingHelix
 {
     public partial class Form1 : Form
     {
+        private ElementHost elementHost;
+        private HelixToolkit.Wpf.SharpDX.Viewport3DX viewport3DX;
         public Form1( )
         {
             InitializeComponent( );
+        }
+
+        private void Form1_Load( object sender, EventArgs e )
+        {
+            elementHost = new ElementHost
+            {
+                Dock = DockStyle.Fill
+            };
+            panel1.Controls.Add( elementHost );
+            viewport3DX = new HelixToolkit.Wpf.SharpDX.Viewport3DX( );
+            elementHost.Child = viewport3DX;
+            var b1 = new MeshBuilder();
+            b1.AddSphere( new Vector3( 0, 0, 0 ), 0.5 );
+            b1.AddBox( new Vector3( 0, 0, 0 ), 1, 0.5, 2, BoxFaces.All );
+            MeshGeometry3D model = b1.ToMeshGeometry3D();
+            MeshGeometryModel3D test = new MeshGeometryModel3D
+            {
+                Material = PhongMaterials.Gold,
+                Geometry = model
+            };
+            var ambientLight = new AmbientLight3D
+            {
+                Color = new Color4( 0.1f, 0.1f, 0.1f, 1.0f )
+            };
+            var directionalLight = new DirectionalLight3D
+            {
+                Color = SharpDX.Color.White,
+                Direction = new Vector3(-2, -5, -2)
+            };
+            viewport3DX.Items.Add( ambientLight );
+            viewport3DX.Items.Add( directionalLight );
+            //test.Attach( viewport3DX );
+            viewport3DX.Items.Add( test );
         }
     }
 }
